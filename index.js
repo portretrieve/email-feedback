b51f15260090c535f9a1232c03fc1a1154c21617
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-07-25 01:23:25 -0400

    updated info

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ title: ["Hi We Something"] });
+  res.send({ title: ["Hi We have added Something"] });
 });
 
 const PORT = process.env.PORT || 3000;
